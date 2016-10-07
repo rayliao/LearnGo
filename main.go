a630ad6bf94f763d30b25b2e1a51872db172a41f
Ray <lens1989@gmail.com>
2016-10-07 23:27:40 +0800

    learning

--- main.go
+++ main.go
@@ -4,6 +4,18 @@ import (
 	"fmt"
 )
 
+const (
+	x = iota
+	y
+	z
+	w
+)
+
 func main() {
-	fmt.Print("Hello, RayLiao.")
+	var c complex64 = 5 + 5i
+	fmt.Print(x)
+	fmt.Print(y)
+	fmt.Print(z)
+	fmt.Print(w)
+	fmt.Printf("Value is: %v", c)
 }
