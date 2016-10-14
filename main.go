9ef1146004b045609a77833e72b251d9dd23b3f7
rayliao <lens1989@gmail.com>
2016-10-14 22:06:39 +0700

    learning

--- main.go
+++ main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"fmt"
-	"os"
 	// "github.com/rayliao/go-learning/demo"
 	// "github.com/rayliao/go-learning/utils"
-	"github.com/rayliao/go-learning/modals"
+	// "github.com/rayliao/go-learning/modals"
 )
 
+func change(arr3 *[3]int) {
+	for i := range arr3 {
+		arr3[i] = 0
+	}
+}
+
 func main() {
-	fmt.Println(modals.Staff{1, 2})
+	// arr1 := [3]int{1, 2, 3}
+	arr2 := &[3]int{2, 3, 4}
+	var arr3 []int
+	var arr4 []int
+	arr3 = append(arr3, 0)
+	copy(arr4, arr3)
+	bt1 := [...]byte{1: 1}
+	fmt.Println(bt1[:] == nil)
+	// fmt.Println(arr1 == arr2)
+	fmt.Println(*arr2)
+	fmt.Println(arr4)
 	// demo.Log("hahha")
 	// demo.money("为什么这样就先不")
 	// utils.Log("User Info:")
