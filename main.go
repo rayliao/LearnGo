41d4c0519b323c91518891b93d0e07cc77a68dec
rayliao <liaoyl@mycode.best>
2016-12-13 17:32:03 +0800

    socker

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/md5"
 	"fmt"
+	"golang.org/x/net/websocket"
 	"html/template"
 	"io"
 	"log"
@@ -61,10 +62,33 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func socket(ws *websocket.Conn) {
+	var err error
+
+	for {
+		var reply string
+		if err = websocket.Message.Receive(ws, &reply); err != nil {
+			fmt.Println("Can't receive")
+			break
+		}
+
+		fmt.Println("Received back from client: " + reply)
+
+		msg := "Received: " + reply
+		fmt.Println("Sending to client: " + msg)
+
+		if err = websocket.Message.Send(ws, msg); err != nil {
+			fmt.Println("Can't send")
+			break
+		}
+	}
+}
+
 func main() {
 	// http.ListenAndServe(config["port"].(string), nil)
 	http.HandleFunc("/", sayHelloName)
 	http.HandleFunc("/login", login)
+	http.Handle("/msg", websocket.Handler(socket))
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
