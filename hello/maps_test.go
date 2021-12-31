f862be402992463ed098924b7dfaa17121e7ec6c
hooray <lens1989@gmail.com>
2021-12-31 16:30:18 +0800

    feat: maps

--- hello/maps_test.go
+++ hello/maps_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func assertStrings(t *testing.T, got, want string) {
 	t.Helper()
@@ -37,12 +39,25 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	dictionary.Add("test", "this is just a test")
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		err := dictionary.Add("test", "this is just a test")
 
-	want := "this is just a test"
+		want := "this is just a test"
 
-	assertDefinition(t, dictionary, "test", want)
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, "test", want)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
 
 func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
@@ -55,5 +70,42 @@ func assertDefinition(t *testing.T, dictionary Dictionary, word, definition stri
 
 	if definition != got {
 		t.Errorf("got %q want %q", got, definition)
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	t.Run("update word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+
+		newDefinition := "new definition"
+
+		err := dictionary.Update(word, newDefinition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, newDefinition)
+	})
+
+	t.Run("update non-existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+		assertError(t, err, ErrWordNonExists)
+	})
+}
+
+func TestDelete(t *testing.T) {
+	word := "test"
+	dictionary := Dictionary{word: "test definition"}
+
+	dictionary.Delete(word)
+
+	_, err := dictionary.Search(word)
+
+	if err != ErrNotFound {
+		t.Errorf("希望 '%s' 是已经被删除了", word)
 	}
 }
