283723403a649b709f7d30e02244a8d19b969ea8
hooray <lens1989@gmail.com>
2022-02-21 16:43:39 +0800

    refactor

---
+++ hello/http/enter.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 1234
+}
+
+func main() {
+	server := &PlayerServer{&InMemoryPlayerStore{}}
+	handler := http.HandlerFunc(server.ServeHTPP)
+
+	if err := http.ListenAndServe(":5000", handler); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
+}
